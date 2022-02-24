8c0d0ccef946f72f20cdad5f929ed240f794ebf4
Shajidur-Rahman <shajidurrahmansaad10@gmail.com>
2022-02-24 11:24:03 +0600

    post component finished. Needs to update later

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import Post from "./components/Post"
+import './App.css';
 
 function App() {
   return (
       <>
+      <div className="post">
+
       <Post id="shajidur" img="./components/img/download.jpeg" comment="Please comment "/>
+      <Post id="shajidur" img="./components/img/download.jpeg" comment="Please comment "/>
+      <Post id="shajidur" img="./components/img/download.jpeg" comment="Please comment "/>
+      <Post id="shajidur" img="./components/img/download.jpeg" comment="Please comment "/>
+      <Post id="shajidur" img="./components/img/download.jpeg" comment="Please comment "/>
+      <Post id="shajidur" img="./components/img/download.jpeg" comment="Please comment "/>
+
+      </div>
       </>
   );
 }
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,18 +1,41 @@
 import React from 'react'
 import './post.css';
 import postImg from "./img/download.jpeg"
+import imoB from "./img/heartB.png"
+import imoR from "./img/heartR.png"
+import { useState } from 'react';
 
-export default function Post() {
+
+export default function Post(props) {
 
     const Poststyle = {
         
     }
 
+    const changeImg = ()=>{
+      console.log("clicked")
+        if (imo === imoB){
+          setImo(imoR)
+        } else{
+          setImo(imoB)
+        }
+    }
+
+    const [imo, setImo] = useState(imoB)
+
   return (
     <>
-    <div className="container" style={Poststyle}>
-        <img src={postImg} alt="post " />
-    </div>
+      <div className="container">
+        <p className='id'>{props.id}</p>
+
+      <div className="img">  
+        <img src={postImg} alt="post image" />
+        </div>
+        <div className="inputDiv">
+        <input className='input' type="text" name="text" id="" placeholder='Enter your comment! '/>
+        <img className='ImgImo' src={imo} alt="" onClick={changeImg}/>
+        </div>
+      </div>
     </>
   )
 }
